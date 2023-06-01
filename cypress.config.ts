2e9a4fb5844355846bfa433f9bb7596ad19ec45d
zahara aviv <zahara.aviv@gmail.com>
2023-05-31 21:47:31 -0400

    baseline cypress testing framework

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: 'kjwsug',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
